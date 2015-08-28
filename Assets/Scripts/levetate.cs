56c2974f034144bcfbec19a564174a578401454f
Alec Arme <Incubusalec@gmail.com>
2015-08-28 14:54:36 -0700

    import crystal masters. Build crystal array prefab. Add filter toggle script bound to 'f'

---
+++ Assets/Scripts/levetate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levetate : MonoBehaviour {
+	public float hoverSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 updatePosition = transform.position;
+		updatePosition.y += Mathf.Sin (Time.time * hoverSpeed) * Time.deltaTime;
+		transform.position = updatePosition;
+	}
+}
---
+++ Assets/filterToggle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class filterToggle : MonoBehaviour {
+	private AlpacaSound.RetroPixel retroPixel;
+	public GameObject fpsCharacter;
+
+	// Use this for initialization
+	void Start () {
+		retroPixel = fpsCharacter.GetComponent<AlpacaSound.RetroPixel> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("f")) 
+		{
+			retroPixel.enabled = !retroPixel.enabled;
+		}
+	}
+}
